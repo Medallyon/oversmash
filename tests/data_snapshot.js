90430b285cace317240c877168a088766b56b1fe
Tilman Raendchen <traendchen@outlook.com>
2021-02-28 03:50:01 +0000

    clearer code

--- tests/data_snapshot.js
+++ tests/data_snapshot.js
@@ -1,6 +1,6 @@
 import { promisify } from 'util'
 import { writeFile } from 'fs'
-import oversmash from '../lib'
+import oversmash from '../lib/index.js'
 
 // Picked top player in overbuff, if this is you and you'd rather not be here,
 // please send me a message!
